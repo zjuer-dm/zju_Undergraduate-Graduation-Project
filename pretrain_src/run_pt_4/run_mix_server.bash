f5362f913a9a526801f8c3835a759c8d01f0c2ef
zjuer-dm <3220105563@zju.edu.cn>
2025-12-26 20:13:35 +0800

    use

--- pretrain_src/run_pt_4/run_mix_server.bash
+++ pretrain_src/run_pt_4/run_mix_server.bash
@@ -1,6 +1,6 @@
 
 NODE_RANK=0
-NUM_GPUS=4
+NUM_GPUS=2
 outdir=pretrained/r2r_rxr_ce_4cam/mlm.sap_habitat_depth
 
 python -m torch.distributed.launch \
@@ -10,5 +10,5 @@ python -m torch.distributed.launch \
     --model_config pretrain_src/run_pt_4/mix_model_config_dep.json \
     --config pretrain_src/run_pt_4/mix_pretrain_server.json \
     --output_dir $outdir
-
-# CUDA_VISIBLE_DEVICES=0,1,2,3 bash pretrain_src/run_pt_4/run_mix_server.bash 2333
+# export NCCL_P2P_DISABLE=1
+# CUDA_VISIBLE_DEVICES=6,7 bash pretrain_src/run_pt_4/run_mix_server.bash 2333
